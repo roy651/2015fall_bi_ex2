1b4f49355e668aefdb9bb74596e23be6b53b57f2
Roy Abitbol <roy@spectory.com>
2016-01-01 22:34:15 +0200

    yet another log

--- main.rb
+++ main.rb
@@ -13,6 +13,8 @@ module EX2Main
     train_header, train_data = Utils.read_data_file(train_file_path)
     test_header, test_data = Utils.read_data_file(test_file_path)
 
+    puts "---------------------------------------------"
+    puts "TRAINING: #{train_file_path}"
     # grow forest
     random_forest = TreeBuilder.build_random_forest(
       train_header,
@@ -27,6 +29,7 @@ module EX2Main
     # print the tree if there's only a single tree - used for debug
     random_forest.first.print('') if random_forest.size == 1
 
+    puts "TESTING: #{test_file_path}"
     # test the forest
     forest_results_per_test_rec, progressive_results_per_tree = 
       TreeTester.test_random_forest(random_forest, test_data, test_header)
@@ -44,11 +47,11 @@ end
 time = Benchmark.measure {
 EX2Main.run('../ex2files/data.txt', '../ex2files/data2.txt', 20, 10, 200, 2, 3, 1) if __FILE__ == $PROGRAM_NAME
 # best=2500~ => 2600
-# EX2Main.run('../ex2files/forestS.txt', '../ex2files/forestS2.txt', 50, 7, 70, 12, 5, 1) if __FILE__ == $PROGRAM_NAME
+EX2Main.run('../ex2files/forestS.txt', '../ex2files/forestS2.txt', 50, 7, 70, 12, 5, 1) if __FILE__ == $PROGRAM_NAME
 # best=2600~ => 3600
-# EX2Main.run('../ex2files/forestR.txt', '../ex2files/forestR2.txt', 30, 10, 100, 14, 5, 1) if __FILE__ == $PROGRAM_NAME
+EX2Main.run('../ex2files/forestR.txt', '../ex2files/forestR2.txt', 30, 10, 100, 14, 5, 1) if __FILE__ == $PROGRAM_NAME
 # best 0.18~ => 0.12
-# EX2Main.run('../ex2files/sphere.txt', '../ex2files/sphere2.txt', 30, 10, 100, 10, 5, 1) if __FILE__ == $PROGRAM_NAME
+EX2Main.run('../ex2files/sphere.txt', '../ex2files/sphere2.txt', 30, 10, 100, 10, 5, 1) if __FILE__ == $PROGRAM_NAME
 # best 0.70~ => 0.57 
 }
 puts 
